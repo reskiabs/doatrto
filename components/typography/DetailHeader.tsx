32c04b428ec098bc6e49cc115c40950f7de09583
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2025-07-20 10:27:10 +0700

    slicing: list article page

---
+++ components/typography/DetailHeader.tsx
@@ -0,0 +1,16 @@
+interface DetailHeaderProps {
+  title: string;
+  description: string;
+}
+const DetailHeader = ({ title, description }: DetailHeaderProps) => {
+  return (
+    <div className="flex flex-col items-center space-y-5">
+      <h1 className="text-tertiary font-bold text-[50px]">{title}</h1>
+      <h1 className="text-tertiary font-normal text-[25px] max-w-[1140px] text-center">
+        {description}
+      </h1>
+    </div>
+  );
+};
+
+export default DetailHeader;
---
+++ src/app/[locale]/article/[id]/page.tsx
@@ -0,0 +1,17 @@
+import DetailHeader from "@/components/typography/DetailHeader";
+
+const ArticleDetailPage = () => {
+  return (
+    <div>
+      <div className="flex flex-col justify-center items-center bg-gradient-to-b from-white via-[#EBF0F8] to-white pt-[104px]">
+        <DetailHeader
+          title="The Latest News from D.O.A TRTO"
+          description="Lebih dari 500 orang sudah terbantu dan merasakan kebahagiaan
+dengan Divine Oracular Assistance Tension Releasing Technique Online"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ArticleDetailPage;
