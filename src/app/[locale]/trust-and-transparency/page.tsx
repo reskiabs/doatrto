23f3da2c42a307ac104b428a9177ec75eb4d4e15
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2025-07-20 14:55:14 +0700

    slicing: testimonials page

--- src/app/[locale]/trust-and-transparency/page.tsx
+++ src/app/[locale]/trust-and-transparency/page.tsx
@@ -1,9 +1,15 @@
-import ComingSoon from "@/components/common/ComingSoon";
+import DetailHeader from "@/components/typography/DetailHeader";
 
 const TrustAndTransparency = () => {
   return (
     <div>
-      <ComingSoon />
+      <div className="flex flex-col justify-center items-center bg-gradient-to-b from-white via-[#EBF0F8] to-white pt-[104px]">
+        <DetailHeader
+          title="Trust and Transparency"
+          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+Lorem Ipsum has been the industry's standard."
+        />
+      </div>
     </div>
   );
 };
