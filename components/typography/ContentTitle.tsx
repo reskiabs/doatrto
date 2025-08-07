596815f216b0b75db3b47d7fc748871035f24af6
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2025-08-07 08:16:19 +0700

    style: mobile view - home - trusted medical expert

--- components/typography/ContentTitle.tsx
+++ components/typography/ContentTitle.tsx
@@ -1,5 +1,9 @@
 const ContentTitle = ({ title }: { title: string }) => {
-  return <p className="text-accent font-extrabold text-[40px]">{title}</p>;
+  return (
+    <p className="text-accent font-extrabold text-[28px] mx-[35px] lg:mx-0 text-center lg:text-[40px]">
+      {title}
+    </p>
+  );
 };
 
 export default ContentTitle;
