2067fd480e0727429d9b67c0ba71658914cc6299
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2025-08-07 09:00:34 +0700

    style: mobile view - home - the differences

--- components/typography/ContentTitle.tsx
+++ components/typography/ContentTitle.tsx
@@ -1,6 +1,6 @@
 const ContentTitle = ({ title }: { title: string }) => {
   return (
-    <p className="text-accent font-extrabold text-[28px] mx-[35px] lg:mx-0 text-center lg:text-[40px]">
+    <p className="text-accent font-extrabold text-[28px] mx-2.5 lg:mx-0 text-center lg:text-[40px]">
       {title}
     </p>
   );
