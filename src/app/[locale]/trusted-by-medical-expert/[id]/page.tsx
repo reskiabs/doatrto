45dad98d2e5ac673b0178aa7225c78f1740478d8
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2025-08-09 08:40:43 +0700

    feat: mobile view - trusted medical experts

--- src/app/[locale]/trusted-by-medical-expert/[id]/page.tsx
+++ src/app/[locale]/trusted-by-medical-expert/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import TrustByMedicalExpert from "@/components/content/TrustedByMedicalExpert";
-import PageHeader from "@/components/typography/PageHeader";
+import DetailHeader from "@/components/typography/DetailHeader";
 import { useTrustedMedical } from "@/hooks/useTrustedMedical";
 import { ImageDown } from "lucide-react";
 import Image from "next/image";
@@ -15,10 +15,10 @@ const TrustedByMedicalExpertDetailPage = () => {
 
   return (
     <div>
-      <div className="flex flex-col justify-center items-center bg-gradient-to-b from-white via-[#EBF0F8] to-white pt-[104px]">
-        <PageHeader
-          pageTitle="TRUSTED BY MEDICAL EXPERT"
-          detailTitle={trustedMedical?.title || "Trusted Medical Expert"}
+      <div className="flex flex-col justify-center items-center bg-gradient-to-b from-white via-[#EBF0F8] to-white pt-[50px] lg:pt-[100px]">
+        <DetailHeader
+          title="Trusted by Medical Experts"
+          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
         />
 
         {loading ? (
@@ -26,7 +26,7 @@ const TrustedByMedicalExpertDetailPage = () => {
             <ImageDown className="animate-bounce text-muted" size={150} />
           </div>
         ) : (
-          <div className="relative w-[1140px] h-[641px] rounded-[50px] overflow-hidden mt-[50px]">
+          <div className="relative size-[372px] lg:w-[1140px] lg:h-[641px] rounded-[15px] lg:rounded-[50px] overflow-hidden mt-[50px]">
             {trustedMedical?.thumbnail && (
               <Image
                 src={trustedMedical.thumbnail}
@@ -41,17 +41,17 @@ const TrustedByMedicalExpertDetailPage = () => {
       </div>
 
       {!loading && trustedMedical && (
-        <div className="mt-[100px] w-full flex flex-col justify-center items-center">
+        <div className="mt-[20px] lg:mt-[100px] px-[15px] lg:px-0 w-full flex flex-col justify-center items-center">
           <div className="w-full max-w-[1140px]">
-            <h1 className="font-medium text-[40px] text-tertiary text-left">
-              Speaker:{" "}
-              <span className="font-extrabold">
-                {trustedMedical.speaker || "—"}
-              </span>
+            <h1 className="font-extrabold text-[28px] lg:text-[40px] text-tertiary text-left">
+              {trustedMedical.title || "—"}
+            </h1>
+            <h1 className="font-medium text-[28px] lg:text-[40px] text-tertiary text-left">
+              Speaker: {trustedMedical.speaker}
             </h1>
           </div>
           <div
-            className="font-normal text-justify text-2xl max-w-[1140px] mt-[30px]"
+            className="text-sm lg:text-xl leading-relaxed text-justify max-w-[1140px] mt-[20px] lg:mt-[30px]"
             dangerouslySetInnerHTML={{
               __html: trustedMedical.description || "",
             }}
