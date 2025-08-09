215cc2e2a8521c16f76b00eeb4f6f6232d8fd91b
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2025-08-09 09:54:44 +0700

    feat: mobile view - the differences

--- src/app/[locale]/the-difference/[id]/page.tsx
+++ src/app/[locale]/the-difference/[id]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import PageHeader from "@/components/typography/PageHeader";
+import DetailHeader from "@/components/typography/DetailHeader";
+import { useMobileScrollOffset } from "@/hooks/useMobileScrollOffset";
 import { useTheDifferences } from "@/hooks/useTheDifferences";
 import { ImageDown } from "lucide-react";
 import Image from "next/image";
@@ -12,23 +13,26 @@ const TheDifferenceDetailPage = () => {
 
   const { items, loading } = useTheDifferences();
   const detail = items.find((item) => item.id.toString() === id);
+  const scrollRef = useMobileScrollOffset(0.22, [detail]);
+
+  const hasImages = Array.isArray(detail?.images) && detail.images.length > 0;
 
   return (
     <div>
       {/* Header */}
-      <div className="flex flex-col justify-center items-center bg-gradient-to-b from-white via-[#EBF0F8] to-white pt-[104px]">
-        <PageHeader
-          pageTitle="THE DIFFERENCE"
-          detailTitle={detail?.title || "The Difference Detail"}
+      <div className="flex flex-col justify-center items-center bg-gradient-to-b from-white via-[#EBF0F8] to-white pt-[50px] lg:pt-[100px]">
+        <DetailHeader
+          title="The Differences"
+          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
         />
 
         {/* Thumbnail */}
         {loading ? (
-          <div className="relative w-[1140px] h-[641px] rounded-[50px] mt-[50px] overflow-hidden bg-surface flex justify-center items-center">
+          <div className="relative size-[372px] lg:w-[1140px] lg:h-[641px] rounded-[15px] lg:rounded-[50px] mt-[50px] overflow-hidden bg-surface flex justify-center items-center">
             <ImageDown className="animate-bounce text-muted" size={150} />
           </div>
         ) : (
-          <div className="relative w-[1140px] h-[641px] rounded-[50px] overflow-hidden mt-[50px]">
+          <div className="relative size-[372px] lg:w-[1140px] lg:h-[641px] rounded-[15px] lg:rounded-[50px] overflow-hidden mt-[50px]">
             {detail?.thumbnail && (
               <Image
                 src={detail.thumbnail}
@@ -44,41 +48,43 @@ const TheDifferenceDetailPage = () => {
 
       {/* Deskripsi */}
       {!loading && detail && (
-        <div className="mt-[100px] w-full flex flex-col justify-center items-center">
+        <div className="mt-[20px] lg:mt-[100px] px-[15px] lg:px-0 flex flex-col justify-center items-center">
           <div className="w-full max-w-[1140px]">
-            <h1 className="font-extrabold text-[40px] text-tertiary text-left">
+            <h1 className="font-extrabold text-[28px] lg:text-[40px] text-tertiary text-center lg:text-left">
               Description
             </h1>
           </div>
           <div
-            className="font-normal text-justify text-2xl max-w-[1140px] mt-[30px]"
+            className="font-normal text-[14px] lg:text-xl text-center lg:text-justify max-w-[1140px] mt-[20px] lg:mt-[30px]"
             dangerouslySetInnerHTML={{ __html: detail.description }}
           />
         </div>
       )}
 
       {/* Galeri Gambar */}
-      {!loading &&
-        Array.isArray(detail?.images) &&
-        detail.images.length > 0 && (
-          <div className="w-full mt-[100px] flex flex-col justify-center items-center">
-            <div className="grid grid-cols-2 gap-[30px] max-w-[1140px]">
-              {detail.images.map((img, i) => (
-                <div
-                  key={i}
-                  className="relative w-[555px] h-[312px] rounded-[25px] overflow-hidden"
-                >
-                  <Image
-                    src={img}
-                    alt={`Difference image ${i + 1}`}
-                    fill
-                    className="object-cover"
-                  />
-                </div>
-              ))}
-            </div>
+      {hasImages && (
+        <div
+          ref={scrollRef}
+          className="my-[50px] lg:my-[100px] px-2.5 lg:px-0 overflow-x-auto scrollbar-hide"
+        >
+          <div className="grid grid-cols-3 gap-[10px] lg:gap-[30px] min-w-max snap-x snap-mandatory">
+            {detail?.images.slice(0, 3).map((image: string, index: number) => (
+              <div
+                key={index}
+                className="relative size-[250px] lg:size-[360px] rounded-[15px] lg:rounded-[25px] overflow-hidden bg-gray-300 snap-start"
+              >
+                <Image
+                  src={image}
+                  alt={`Image ${index + 1}`}
+                  fill
+                  priority
+                  className="object-cover"
+                />
+              </div>
+            ))}
           </div>
-        )}
+        </div>
+      )}
     </div>
   );
 };
