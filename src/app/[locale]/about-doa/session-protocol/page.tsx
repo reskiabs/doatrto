406e18a88f45def593fcbfa042b72722fcd0b7c9
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2025-08-09 10:46:27 +0700

    feat: mobile view - about doa - informational pages

--- src/app/[locale]/about-doa/session-protocol/page.tsx
+++ src/app/[locale]/about-doa/session-protocol/page.tsx
@@ -5,22 +5,22 @@ import { gridImages } from "@/data/grid-image";
 const SessionProtocolPage = () => {
   return (
     <div>
-      <div className="flex flex-col justify-center items-center bg-gradient-to-b from-white via-[#EBF0F8] to-white pt-[104px]">
+      <div className="flex flex-col justify-center items-center bg-gradient-to-b from-white via-[#EBF0F8] to-white pt-[50px] lg:pt-[100px]">
         <DetailHeader
           title="Session Protocol"
           description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
         />
-        <div className="mt-[100px]">
+        <div className="mt-[50px] lgmt-[100px]">
           <ImageGrid images={gridImages} />
         </div>
       </div>
-      <div className="mt-[100px] w-full flex flex-col justify-center items-center">
+      <div className="mt-[50px] lg:mt-[100px] px-[15px] lg:px-0 w-full flex flex-col justify-center items-center">
         <div className="w-full max-w-[1140px]">
-          <h1 className="font-extrabold text-[40px] text-tertiary text-left">
+          <h1 className="font-extrabold text-left text-[28px] lg:text-[40px] text-tertiary">
             Lorem ipsum dolor sit amet
           </h1>
         </div>
-        <p className="font-normal text-justify text-2xl max-w-[1140px] mt-[30px]">
+        <p className="font-normal text-justify text-sm lg:text-xl max-w-[1140px] mt-[20px] lg:mt-[30px]">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry&apos;s standard dummy text
           ever since the 1500s, when an unknown printer took a galley of type
