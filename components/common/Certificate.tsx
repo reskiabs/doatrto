3d975849f2ebeb6c134abbc5f3fb8570c065913a
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2025-08-28 13:13:19 +0700

    feat: update styling

--- components/common/Certificate.tsx
+++ components/common/Certificate.tsx
@@ -41,33 +41,35 @@ const certList = [
 
 const Certificate = () => {
   return (
-    <div className="flex flex-col justify-center items-center mt-[50px] lg:mt-[100px]">
-      <div className="py-3 w-full  lg:py-6 justify-center flex bg-surface overflow-hidden">
-        <div className="animate-scroll-slow flex justify-center items-center gap-[25px] lg:gap-[50px]">
-          {[...certList, ...certList].map((item, index) => (
-            <div
-              key={index}
-              className="flex flex-col items-center min-w-[200px] lg:min-w-[250px]"
-            >
-              <div className="relative size-[50px] lg:size-[80px] lg:mb-1.5">
-                <Image
-                  src={item.image}
-                  alt={item.title}
-                  fill
-                  className="object-contain"
-                />
+    <main className="flex flex-col items-center w-full mt-[30px] md:mt-[60px] lg:mt-[80px] overflow-x-hidden">
+      <div className="w-full pt-3 pb-1.5 lg:pt-6 lg:pb-6 bg-surface flex flex-col items-center justify-center">
+        <div className="-mt-1 lg:mt-2 w-full overflow-hidden">
+          <div className="animate-scroll-fast lg:animate-scroll-slow flex gap-[25px] lg:gap-0">
+            {[...certList, ...certList].map((item, index) => (
+              <div
+                key={index}
+                className="flex flex-col items-center min-w-[200px] lg:min-w-[350px]"
+              >
+                <div className="relative size-[50px] lg:size-[80px] lg:mb-1.5">
+                  <Image
+                    src={item.image}
+                    alt={item.title}
+                    fill
+                    className="object-contain"
+                  />
+                </div>
+                <p className="text-xs lg:text-lg font-bold text-center text-body">
+                  {item.title}
+                </p>
+                <p className="text-2xs lg:text-sm -mt-0.5 font-normal text-center text-muted">
+                  {item.subtitle}
+                </p>
               </div>
-              <p className="text-xs lg:text-lg font-bold text-center text-body">
-                {item.title}
-              </p>
-              <p className="text-2xs lg:text-sm -mt-0.5 font-normal text-center text-muted line-clamp-1">
-                {item.subtitle}
-              </p>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 
