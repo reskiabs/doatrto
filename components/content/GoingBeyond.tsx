25ecd2c074cb0a2aa8899b63c65ba3aa163e7469
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2025-08-28 10:43:40 +0700

    feat: update beyond

--- components/content/GoingBeyond.tsx
+++ components/content/GoingBeyond.tsx
@@ -1,34 +1,18 @@
 "use client";
 
+import LoaderContent from "@/components/common/LoaderContent";
+import SomethingWentWrong from "@/components/common/SomethingWentWrong";
+import { useBeyond } from "@/hooks/useBeyond";
 import { useMobileScrollOffset } from "@/hooks/useMobileScrollOffset";
-import Flower from "@/public/images/beyond1.svg";
-import Eye from "@/public/images/beyond2.svg";
-import Repeated from "@/public/images/beyond3.svg";
-import Lightning from "@/public/images/beyond4.svg";
-import Violently from "@/public/images/beyond5.svg";
-import Heal from "@/public/images/beyond6.svg";
-import { useTranslations } from "next-intl";
-import { StaticImageData } from "next/image";
 import BeyondCardList from "../card/BeyondCardList";
 import ContentTitle from "../typography/ContentTitle";
 
-type Item = {
-  id: number;
-  image: StaticImageData | string;
-};
-
-const items: Item[] = [
-  { id: 1, image: Flower },
-  { id: 2, image: Eye },
-  { id: 3, image: Repeated },
-  { id: 4, image: Lightning },
-  { id: 5, image: Violently },
-  { id: 6, image: Heal },
-];
-
 const GoingBeyond = () => {
   const scrollRef = useMobileScrollOffset(0.17);
-  const t = useTranslations("items");
+  const { beyond, loading, error } = useBeyond();
+
+  if (loading) return <LoaderContent />;
+  if (error) return <SomethingWentWrong />;
 
   return (
     <main className="flex flex-col items-center w-full mt-[30px] md:mt-[60px] lg:mt-[80px]">
@@ -39,12 +23,12 @@ const GoingBeyond = () => {
 
         <div ref={scrollRef} className="w-full overflow-x-auto scrollbar-hide">
           <div className="grid grid-cols-3 gap-3.5 lg:gap-6 min-w-max snap-x snap-mandatory px-[15px] lg:px-0">
-            {items.map((item) => (
+            {beyond.map((item) => (
               <div key={item.id} className="snap-start">
                 <BeyondCardList
-                  title={t(`${item.id}.title`)}
-                  description={t(`${item.id}.description`)}
-                  image={item.image}
+                  title={item.title}
+                  description={item.description}
+                  image={item.thumbnail}
                 />
               </div>
             ))}
